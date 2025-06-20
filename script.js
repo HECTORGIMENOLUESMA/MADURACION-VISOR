3576305ed7eb608057ec02f26121ce3dc8bd37a3
HECTORGIMENOLUESMA <tecnicocampo@bodegascare.com>
2025-06-20 13:30:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// JS de prueba inicial
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("Visor cargado");
-});
+
+function filtrarDatos() {
+  document.getElementById("tabla-resultados").innerHTML = "<p>[Simulación] Resultados filtrados mostrados aquí.</p>";
+  document.getElementById("chart-grado-acidez-ph").innerText = "[Gráfica Grado - Acidez - pH]";
+  document.getElementById("chart-brix-solutos").innerText = "[Gráfica Brix - Solutos]";
+  document.getElementById("chart-azucar").innerText = "[Gráfica Azúcar]";
+  document.getElementById("chart-imf").innerText = "[Gráfica IMF]";
+  document.getElementById("chart-maturox").innerText = "[Gráfica MATUROX]";
+}
