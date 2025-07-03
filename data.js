40d853fcd8e52d68e8585f50b622f959a5655fac
HECTORGIMENOLUESMA <tecnicocampo@bodegascare.com>
2025-07-03 13:56:40 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const muestreos = [
+  {
+    "BODEGA": "CARE",
+    "VITICULTOR": "JUAN",
+    "A\u00d1O": 2024,
+    "ZONA": "BANCALES",
+    "VARIEDAD": "CHARDONNAY",
+    "PUNTO MUESTREO": "FILAS 25-26",
+    "BAUM\u00c9": 11.5,
+    "AC TOTAL": 5.3,
+    "pH": 3.2,
+    "YAN": 180
+  }
+];
